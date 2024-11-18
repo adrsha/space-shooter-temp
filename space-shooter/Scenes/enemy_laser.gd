612f63d710f70ac623dbd0d0386a1b54a58c8442
adrsha <adrsha@github.com>
2024-11-18 15:24:47 +0545

    addition: for dipak

---
+++ space-shooter/Scenes/enemy_laser.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var speed = 1000
+var window_height = DisplayServer.window_get_size().y
+
+# Called when the node is first added to the scene
+func _ready():
+	area_entered.connect(_on_body_entered)
+
+# Called every physics frame (1/60th of a second by default)
+func _physics_process(delta):
+	position.y += speed * delta
+	
+	# Remove the laser if it goes too far up
+	if position.y < -window_height:
+		queue_free()  # Deletes the laser node
+
+# This function runs whenever another Area2D touches our laser
+func _on_body_entered(body):
+	# Check if what we hit is an enemy
+	if body.get_parent().is_in_group("player"):
+		print(body)
+		body.get_parent().take_damage()  # Call take_damage on the enemy
+		queue_free()       # Remove the laser
